82d176f4df342e444a8067713a46b5207f81ec1a
Kevin Moore <kevmoo@google.com>
2015-04-22 22:47:18 -0700

    eliminate static warnings in validate_fail_test using 'as dynamic'

--- test/validate_fail_test.dart
+++ test/validate_fail_test.dart
@@ -36,23 +36,23 @@ void main() {
 
     PbList<Int64> lSint64 = new PbSint64List();
     expect(() {
-      lSint64.add(-9223372036854775809);
+      lSint64.add(-9223372036854775809 as dynamic);
     }, badArgument);
     expect(() {
-      lSint64.add(9223372036854775808);
+      lSint64.add(9223372036854775808 as dynamic);
     }, badArgument);
 
     PbList<Int64> lUint64 = new PbUint64List();
     expect(() {
-      lUint64.add(-1);
+      lUint64.add(-1 as dynamic);
     }, badArgument);
     expect(() {
-      lUint64.add(18446744073709551616);
+      lUint64.add(18446744073709551616 as dynamic);
     }, badArgument);
 
     PbList<double> lFloat = new PbFloatList();
     expect(() {
-      lFloat.add(-3.4028234663852886E39);
+      lFloat.add(-3.4028234663852886E39 as dynamic);
     }, throwsArgumentError);
     expect(() {
       lFloat.add(3.4028234663852886E39);
@@ -67,7 +67,7 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalInt32 = '101';
+      builder.optionalInt32 = '101' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalInt32 = -2147483649;
@@ -77,17 +77,17 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalInt64 = '102';
+      builder.optionalInt64 = '102' as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalInt64 = -9223372036854775809;
+      builder.optionalInt64 = -9223372036854775809 as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalInt64 = 9223372036854775808;
+      builder.optionalInt64 = 9223372036854775808 as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalUint32 = '103';
+      builder.optionalUint32 = '103' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalUint32 = -1;
@@ -97,17 +97,17 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalUint64 = '104';
+      builder.optionalUint64 = '104' as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalUint64 = -1;
+      builder.optionalUint64 = -1 as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalUint64 = 18446744073709551616;
+      builder.optionalUint64 = 18446744073709551616 as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalSint32 = '105';
+      builder.optionalSint32 = '105' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalSint32 = -2147483649;
@@ -117,17 +117,17 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalSint64 = '106';
+      builder.optionalSint64 = '106' as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalSint64 = -9223372036854775809;
+      builder.optionalSint64 = -9223372036854775809 as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalSint64 = 9223372036854775808;
+      builder.optionalSint64 = 9223372036854775808 as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalFixed32 = '107';
+      builder.optionalFixed32 = '107' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalFixed32 = -1;
@@ -137,17 +137,17 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalFixed64 = '108';
+      builder.optionalFixed64 = '108' as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalFixed64 = -1;
+      builder.optionalFixed64 = -1 as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalFixed64 = 18446744073709551616;
+      builder.optionalFixed64 = 18446744073709551616 as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalSfixed32 = '109';
+      builder.optionalSfixed32 = '109' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalSfixed32 = -2147483649;
@@ -157,17 +157,17 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalSfixed64 = '110';
+      builder.optionalSfixed64 = '110' as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalSfixed64 = -9223372036854775809;
+      builder.optionalSfixed64 = -9223372036854775809 as dynamic;
     }, badArgument);
     expect(() {
-      builder.optionalSfixed64 = 9223372036854775808;
+      builder.optionalSfixed64 = 9223372036854775808 as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalFloat = '111';
+      builder.optionalFloat = '111' as dynamic;
     }, badArgument);
     expect(() {
       builder.optionalFloat = -3.4028234663852886E39;
@@ -177,32 +177,32 @@ void main() {
     }, throwsArgumentError);
 
     expect(() {
-      builder.optionalDouble = '112';
+      builder.optionalDouble = '112' as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalBool = '113';
+      builder.optionalBool = '113' as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalString = false;
+      builder.optionalString = false as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalBytes = '115';
+      builder.optionalBytes = '115' as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalNestedMessage = '118';
+      builder.optionalNestedMessage = '118' as dynamic;
     }, badArgument);
 
     expect(() {
-      builder.optionalNestedEnum = '121';
+      builder.optionalNestedEnum = '121' as dynamic;
     }, badArgument);
 
     // Set repeating value (no setter should exist).
     expect(() {
-      builder.repeatedInt32 = 201;
+      (builder as dynamic).repeatedInt32 = 201 as dynamic;
     }, throwsNoSuchMethodError);
 
     // Unknown tag.
